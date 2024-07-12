9e1494dab6cd002b8a4c513230f46463734a18d4
tunluo <2602316693@qq.com>
2024-07-12 20:06:21 +0800

    yuoj-backend-judge-service-inital-commit

---
+++ yuoj-backend-judge-service/src/main/java/com/yupi/yuojbackendjudgeservice/judge/codesandbox/CodeSandbox.java
@@ -0,0 +1,9 @@
+package com.yupi.yuojbackendjudgeservice.judge.codesandbox;
+
+
+import com.yupi.yuojbackendmodel.model.codesandbox.ExecuteCodeRequest;
+import com.yupi.yuojbackendmodel.model.codesandbox.ExecuteCodeResponse;
+
+public interface CodeSandbox {
+    ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest);
+}
